9e509632e1ed723f63da693b3731ffaae0adc783
trirahmataribowo <Tri.aribowo@btpn.com>
2019-05-21 22:42:21 +0700

    update backend springboot path param

--- backend/restfull-web-services/src/main/java/com/tra26/rest/webservices/restfullwebservices/HelloWorldController.java
+++ backend/restfull-web-services/src/main/java/com/tra26/rest/webservices/restfullwebservices/HelloWorldController.java
@@ -1,18 +1,25 @@
 package com.tra26.rest.webservices.restfullwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-//controller
+//Controller
 @RestController
 public class HelloWorldController {
-	//GET
-	//URL - /hello-world
-	//method - "Hello World"
-	
-	@RequestMapping(method = RequestMethod.GET, path = "hello-world")
+
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Tri Rahmat Aribowo");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
